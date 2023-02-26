d634ccf132f0f954aec968e34154bae35665add6
KunjJansari <kunj.jansari@bacancy.com>
2023-02-26 12:45:26 +0530

    Existing Problem is solved by different method/logic

--- subtract.rb
+++ subtract.rb
@@ -1,16 +1,40 @@
+# class Subtract
+#   def calc(number)
+#     sum1,sum2 = 0,0
+#     for i in (1..number)
+#       sum1+=i 
+#     end
+#     operand1 = sum1**2
+#     for i in (1..number)
+#       sum2 = sum2+i**2
+#     end
+#     operand2 = sum2
+#     result = operand1 - operand2
+#   end
+# end
+# t1 = Subtract.new
+# p t1.calc(5)
+
+
 class Subtract
-  def calc(number)
-    sum1,sum2 = 0,0
-    for i in (1..number)
-      sum1+=i 
+  def initialize number
+    @number1 = number
+  end
+  def subtract
+    sum1=0
+    sum2=0
+    for op1 in (0..@number1)
+      sum1 += op1
     end
-    operand1 = sum1**2
-    for i in (1..number)
-      sum2 = sum2+i**2
+    sum1 **=2
+    for op2 in (0..@number1)
+      sum2 += op2**2
     end
-    operand2 = sum2
-    result = operand1 - operand2
+    result = sum1 - sum2
+    puts result
   end
 end
-t1 = Subtract.new
-p t1.calc(5)
+puts "Enter number for subtract square of sum and sum of square"
+number = gets.chomp.to_i
+sub = Subtract.new number
+sub.subtract
